7ed34d2724d7fdd621f72fd291cac48f75a63c0c
Denis Benato <benato.denis96@gmail.com>
2024-06-09 22:01:45 +0200

    Stop renaming initramfs and vmlinuz files
    The new frzr can handle arbitrary names for initramfs and vmlinuz: instead of renaming them at build-time do the rename only at install-time and only if the old frzr is being used

--- build-image.sh
+++ build-image.sh
@@ -196,12 +196,6 @@ if [ -e "/boot/initramfs-${KERNEL_PACKAGE}-fallback.img" ]; then
 	rm "/boot/initramfs-${KERNEL_PACKAGE}-fallback.img"
 fi
 
-# move kernel image and initrd to a defualt location if "linux" is not used
-if [ ${KERNEL_PACKAGE} != 'linux' ] ; then
-	mv /boot/vmlinuz-${KERNEL_PACKAGE} /boot/vmlinuz-linux
-	mv /boot/initramfs-${KERNEL_PACKAGE}.img /boot/initramfs-linux.img
-fi
-
 # clean up/remove unnecessary files
 rm -rf \
 /own_pkgs \
