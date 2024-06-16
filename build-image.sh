30ba200c142189d7897f293c98e6024ddb06ce39
Denis Benato <benato.denis96@gmail.com>
2024-06-16 21:47:51 +0200

    Revert "Remove mkinitcpio"
    This reverts commit 42cbb4f5f46fdf5d60a2dec3b47d0e2c64c501a1.

--- build-image.sh
+++ build-image.sh
@@ -99,7 +99,6 @@ sed -i '/CheckSpace/s/^/#/g' /etc/pacman.conf
 pacman --noconfirm -Syy
 
 pacman -S --noconfirm dracut
-pacman -R --noconfirm mkinitcpio
 
 # Install dracut and refactored frzr before the kernel
 pacman --noconfirm -U --overwrite '*' /own_pkgs/chimeraos-dracut-*
