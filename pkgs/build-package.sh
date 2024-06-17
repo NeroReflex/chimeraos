a2881dd8b8db15735188fdc8b39e96557d9dc3b0
Denis Benato <benato.denis96@gmail.com>
2024-06-17 02:59:18 +0200

    Remove mkinitcpio beforehand

--- pkgs/build-package.sh
+++ pkgs/build-package.sh
@@ -3,7 +3,8 @@
 set -x
 
 sudo chown -R build:build /workdir/pkgs
-
+sudo pacman -S --noconfirm dracut
+sudo pacman -R --noconfirm mkinitcpio
 PIKAUR_CMD="PKGDEST=/workdir/pkgs pikaur --noconfirm --build-gpgdir /etc/pacman.d/gnupg -S -P /workdir/${1}/PKGBUILD"
 PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
 "${PIKAUR_RUN[@]}"
