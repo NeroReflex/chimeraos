2baadac52c86f4572c6145e7e064164d122724ac
Denis Benato <benato.denis96@gmail.com>
2024-06-19 17:51:19 +0200

    --no-hostonly

--- pkgs/chimeraos-dracut-cfg/dracut-install.sh
+++ pkgs/chimeraos-dracut-cfg/dracut-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-args=('--force' '--no-hostonly-cmdline' '--strip' '--aggressive-strip' '--reproducible')
+args=('--force' '--strip' '--aggressive-strip' '--reproducible')
 
 while read -r line; do
 	if [[ "$line" == 'usr/lib/modules/'+([^/])'/pkgbase' ]]; then
@@ -9,7 +9,7 @@ while read -r line; do
 		kver="${kver%'/pkgbase'}"
 
 		install -Dm0644 "/${line%'/pkgbase'}/vmlinuz" "/boot/vmlinuz-${pkgbase}"
-		dracut "${args[@]}" "/boot/initramfs-${pkgbase}.img" --kver "$kver"
+		dracut --no-hostonly --no-hostonly-cmdline "${args[@]}" "/boot/initramfs-${pkgbase}.img" --kver "$kver"
 		#dracut --no-hostonly "${args[@]}" "/boot/initramfs-${pkgbase}-fallback.img" --kver "$kver"
 	fi
 done
