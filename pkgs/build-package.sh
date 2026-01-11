2df05ccac336694ed094684c161570acfef85995
Denis Benato <denis.benato@linux.dev>
2026-01-11 16:32:55 +0100

    Fix paru cmd

--- pkgs/build-package.sh
+++ pkgs/build-package.sh
@@ -10,6 +10,6 @@ sudo pacman -S --noconfirm dracut
 sudo pacman -R --noconfirm mkinitcpio
 
 # Build package from local PKGBUILD
-sudo -u build bash -c "cd /workdir/${1} && PKGDEST=/workdir/pkgs paru -Bi --needed --noconfirm -f"
+sudo -u build bash -c "cd /workdir/${1} && PKGDEST=/workdir/pkgs paru -Bi --needed --noconfirm ."
 # remove any epoch (:) in name, replace with -- since not allowed in artifacts
 find /workdir/pkgs/*.pkg.tar* -type f -name '*:*' -execdir bash -c 'mv "$1" "${1//:/--}"' bash {} \;
